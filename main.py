f96f308b24c17085687cf68110cdfc7c1e4bdfca
aryzach <zach_asmith@yahoo.com>
2020-10-06 10:10:47 -0700

    changed main for testing

--- main.py
+++ main.py
@@ -1,3 +1,46 @@
+from ota_update.main.ota_updater import OTAUpdater
+
+
+def download_and_install_update_if_available():
+     o = OTAUpdater('https://github.com/aryzach/valveESP')
+     o.download_and_install_update_if_available('twistedfields', 'alwaysbekind')
+
+
+def start():
+     # your custom code goes here. Something like this: ...
+     # from main.x import YourProject
+     # project = YourProject()
+     # ...
+     print('hi')
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('', 80))
+    s.listen(5)
+
+    while True:
+        download_and_install_update_if_available()
+        conn, addr = s.accept()
+        print('Got a connection from %s' % str(addr))
+        request = conn.recv(1024)
+        request = str(request)
+        print('Content = %s' % request)
+        led_on = request.find('/?led=on')
+        led_off = request.find('/?led=off')
+        if led_on == 6:
+            print('LED ON')
+            led.value(1)
+        if led_off == 6:
+            print('LED OFF')
+            led.value(0)
+        response = web_page()
+        conn.send('HTTP/1.1 200 OK\n')
+        conn.send('Content-Type: text/html\n')
+        conn.send('Connection: close\n\n')
+        conn.sendall(response)
+        conn.close()
+
+
+
+
 def web_page():
   if led.value() == 1:
     gpio_state="ON"
@@ -13,29 +56,12 @@ def web_page():
   <p><a href="/?led=off"><button class="button button2">OFF</button></a></p></body></html>"""
   return html
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('', 80))
-s.listen(5)
 
-while True:
-  conn, addr = s.accept()
-  print('Got a connection from %s' % str(addr))
-  request = conn.recv(1024)
-  request = str(request)
-  print('Content = %s' % request)
-  led_on = request.find('/?led=on')
-  led_off = request.find('/?led=off')
-  if led_on == 6:
-    print('LED ON')
-    led.value(1)
-  if led_off == 6:
-    print('LED OFF')
-    led.value(0)
-  response = web_page()
-  conn.send('HTTP/1.1 200 OK\n')
-  conn.send('Content-Type: text/html\n')
-  conn.send('Connection: close\n\n')
-  conn.sendall(response)
-  conn.close()
 
+def boot():
+     download_and_install_update_if_available()
+     start()
+
+
+boot()
 
